263ce7165e0b4809bcc94e8992e9623c795704a2
ranjan3@gatech.edu <ranjansv@gatech.edu>
2022-06-23 11:16:28 -0500

    Clean up

---
+++ config/random-read-exp.sh
@@ -0,0 +1,12 @@
+BENCH_TYPE="writer-reader"
+ENGINE="daos-array ior+dfs"
+#Data per writer rank per iteration in MB
+DATA_PER_RANK="64"
+#Read IO size in bytes
+READ_IO_SIZE="1024"
+READ_PATTERN="random"
+STEPS=8
+#Number of writer ranks
+PROCS="28"
+#Number of reader ranks = $PROCS/$READ_WRITE_RATIO
+READ_WRITE_RATIO=1
